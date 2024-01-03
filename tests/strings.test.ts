fd82c5530c29c65824cb0b81723ccde385db0589
Samantha Nguyen <contact@samanthanguyen.me>
2024-01-03 02:38:46 -0600

    tests: add test case + comments

--- tests/strings.test.ts
+++ tests/strings.test.ts
@@ -17,11 +17,14 @@ test.each([
 		(cp: string) => isAsciiAlpha(cp),
 		['test', 4],
 	],
-	[
+	[ // collect nothing if position > length of string
 		'test',
 		5,
 		(cp: string) => isAsciiAlpha(cp),
 		['', 5],
+	],
+	[ // collect nothing if string is empty
+		'', 0, () => true, ['', 0],
 	],
 ])('collect a sequence of codepoints', (value, position, predicate, expected) => {
 	expect(collectCodepoints(value, position, predicate)).toEqual(expected);
